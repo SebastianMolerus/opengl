440dfa791e0f5cd02c9919ce6a376c49d6173e08
Sebastian <sebastian.molerus@gmail.com>
2025-07-09 21:15:21 +0200

    camera

--- src/isogame.cpp
+++ src/isogame.cpp
@@ -17,6 +17,50 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
 
+glm::vec3 camera_pos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 const camera_up = glm::vec3{ 0.0f, 1.0f, 0.0f };
+glm::vec3 camera_front{0.0f, 0.0f, -1.0f};
+
+float delta{};
+float yaw{-90.0f};
+float pitch{};
+
+float lastX = SCR_WIDTH /2.0f, lastY = SCR_HEIGHT /2.0f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    static bool firstMouse{true};
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 dir{};
+    dir.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    dir.y = sin(glm::radians(pitch));
+    dir.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    camera_front = glm::normalize(dir);
+}
+
 GLFWwindow* init()
 {
     glfwInit();
@@ -29,6 +73,9 @@ GLFWwindow* init()
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     int const glad{ gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) };
     assert(glad != 0);
@@ -89,43 +136,35 @@ int main()
     vao vao{ "32" , cube };
     shader s{ "1.vert", "1.frag"};
     s.sampler_to_texture("container", "container.jpg");
-
-
+    s.sampler_to_texture("face", "awesomeface.png", GL_RGBA);
 
     glm::mat4 projection = glm::mat4(1.0f);
     projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
     s.set_mat("projection", projection);
 
-    glm::vec3 camera_pos = glm::vec3(0.0f, 0.0f, 3.0f);
-
-    glm::vec3 camera_target = glm::vec3{ 0.0f, 0.0f, 0.0f };
-    glm::vec3 camera_dir = glm::normalize(camera_pos - camera_target);
-
-    glm::vec3 up = glm::vec3{ 0.0f, 1.0f, 0.0f };
-    glm::vec3 camera_right = glm::normalize(glm::cross(up, camera_dir));
-
-    glm::vec3 cameraUp = glm::cross(camera_dir, camera_right);
-
+    float last{};
     while (!glfwWindowShouldClose(window))
     {
+        float const current{ (float)glfwGetTime() };
+        delta = current - last;
+        last = current;
+
         processInput(window);
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
-        s.set_mat("model", model);
-
         glm::mat4 view = glm::mat4(1.0f);
-        float rad = 10.0f;
-        float cam_x = sin(glfwGetTime()) * rad;
-        float cam_z = cos(glfwGetTime()) * rad;
-        view = glm::lookAt(glm::vec3(cam_x, 0.0f, cam_z), glm::vec3{ 0 }, glm::vec3{ 0.0f, 1.0f, 0.0f });
+        view = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
         s.set_mat("view", view);
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (int i = 0; i < 10; ++i) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(i, i, -3.0f));
+            s.set_mat("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -139,6 +178,23 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    const float cameraSpeed{ 2.5f * delta };
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera_pos += camera_front * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera_pos -= camera_front * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera_pos += glm::cross(camera_front, camera_up) * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera_pos -= glm::cross(camera_front, camera_up) * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera_pos += camera_up * cameraSpeed;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
--- src/shaders/1.frag
+++ src/shaders/1.frag
@@ -2,9 +2,10 @@
 out vec4 FragColor;
 
 in vec2 o_texture_coord;
+uniform sampler2D face;
 uniform sampler2D container;
 
 void main()
 {
-    FragColor = texture(container, o_texture_coord);
+    FragColor = mix(texture(container, o_texture_coord), texture(face, o_texture_coord), 0.2);
 }
